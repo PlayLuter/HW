7a1250ec9e9f4e5f9405238f393b6b4ba2d5e7a8
vitopapu4i <149488103+vitopapu4i@users.noreply.github.com>
2024-12-06 10:33:28 +0300

    .

---
+++ K3_7.1/кз 7.1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace ConsoleApp
+{
+    //преобразование строки в число
+    public class StringToInt
+    {
+        public static int Convert(string input)
+        {
+            return int.Parse(input);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("введите число:");
+            string number=Console.ReadLine();
+
+            Console.WriteLine();
+            try
+            {
+                int convertedValue = StringToInt.Convert(number);
+                Console.WriteLine($"введённое число: {convertedValue}");
+            }
+            catch (OverflowException ex)
+            {Console.WriteLine($"число вне диапазона: {ex.Message}");}
+
+
+            catch (FormatException ex)
+            {Console.WriteLine($"введено не число: {ex.Message}");}
+          
+            
+
+
+        }
+    }
+}
---
+++ K3_7.1/кз 7.1/Tests.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+namespace ConsoleApp.Tests
+{
+    [TestClass]
+    public class Tests
+    {
+
+
+        [TestMethod]
+        public void TestZeroConversion()
+        {
+            var actualResult = StringToInt.Convert("0");
+            Assert.AreEqual(0, actualResult);
+        }
+
+        [TestMethod]
+        public void TestPositiveBoundaryConversion()
+        {
+            var actualResult = StringToInt.Convert(int.MaxValue.ToString());
+            Assert.AreEqual(int.MaxValue, actualResult);}
+        [TestMethod]
+        public void TestNegativeBoundaryConversion()
+        {
+            var actualResult = StringToInt.Convert(int.MinValue.ToString());
+            Assert.AreEqual(int.MinValue, actualResult);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(FormatException))]
+        public void TestWhitespaceInput()
+        {
+            StringToInt.Convert(" ");
+        }
+        [TestMethod]
+        [ExpectedException(typeof(FormatException))]
+        public void TestMixedCharactersAndNumbers()
+        {
+            StringToInt.Convert("123abc");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(OverflowException))]
+        public void TestExceedingMaxValueByOne()
+        {
+            unchecked
+            {
+                StringToInt.Convert((int.MaxValue + 1u).ToString());
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(OverflowException))]
+        public void TestExceedingMinValueByOne()
+        {
+            unchecked
+            {
+                StringToInt.Convert((int.MinValue - 1).ToString());
+            }
+        }
+    }
+}
